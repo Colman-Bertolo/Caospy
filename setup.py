c0bfc3b056ddf0017d9d513d9ba193651c70878b
Sebastián Bertolo <sebastian.bertolo@mi.unc.edu.ar>
2021-12-16 11:49:59 -0300

    new setup..py

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
+import os
+import pathlib
+
 from setuptools import setup
 
-# Descripción del proyecto
-with open("README.md") as fp:
-    LONG_DESCRIPTION = fp.read()
+# =============================================================================
+# CONSTANTS
+# =============================================================================
+
+PATH = pathlib.Path(os.path.abspath(os.path.dirname(__file__)))
 
 # Requerimientos
 
 REQUIREMENTS = ["numpy", "pandas", "scipy", "sympy", "matplotlib"]
+
+# Version
+with open(PATH / "caospy" / "__init__.py") as fp:
+    for line in fp.readlines():
+        if line.startswith("__version__ = "):
+            VERSION = line.split("=", 1)[-1].replace('"', "").strip()
+            break
+
+
+# Descripción del proyecto
+with open("README.md") as fp:
+    LONG_DESCRIPTION = fp.read()
 
 # =============================================================================
 # FUNCTIONS
@@ -14,14 +31,14 @@ REQUIREMENTS = ["numpy", "pandas", "scipy", "sympy", "matplotlib"]
 
 setup(
     name="Caospy",
-    version="0.0.1",
+    version=VERSION,
     description="Dynamic systems analysis",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     license="The MIT License",
     author="Juan Colman, Sebastián Bertolo",
     author_email="juancolmanot@gmail.com",
-    url="https://github.com/Colman-Bertolo-DiSCSI2021/Caospy",
+    url="https://github.com/Colman-Bertolo/Caospy",
     packages=["caospy"],
     install_requires=REQUIREMENTS,
     keywords=["caospy", "dynamic system", "chaos"],
